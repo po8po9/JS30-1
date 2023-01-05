1805a8a1c20de1e7813fe61aaa98c5ff101de069
faust-mz <faust.mz@protonmail.com>
2023-01-05 11:30:53 +0100

    works, small delay on repeated keys.

--- script.js
+++ script.js
@@ -3,16 +3,13 @@ const keys ={
 	
 	let modifyKey = document.querySelectorAll(`[data-key="${kCode}"`);
 	modifyKey[1].play();
-
-	let toggleKey = function(){
-	    modifyKey[0].classList.toggle('playing');
-	};
-	toggleKey();
-	setTimeout (toggleKey, 80);
-
-
+	modifyKey[0].classList.toggle('playing');
+	
     },
-    
+
+    endEffect(kCode){
+	document.querySelector(`./keys.[data-key="${kCode}"`).classList.toggle('playing');
+    }
 };
 
 const keyHandler = (ev) => {
@@ -20,8 +17,9 @@ const keyHandler = (ev) => {
     if (!(document.querySelector(`[data-key="${ev.keyCode}"`))){
 	return null;
     }
+    if (ev.keyCode == 'keyup'){return (keys.endEffect(ev.keyCode))}
     return (keys.effect(ev.keyCode));
 };
 
-document.body.addEventListener ('keydown', keyHandler);
+['keydown', 'keyup'].forEach((event) =>{document.body.addEventListener (event, keyHandler);});
 
